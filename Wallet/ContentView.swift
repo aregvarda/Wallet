2db1890b75302635f7f18140b9e18bde1a95435d
Areg Vardanian <aregv@bk.ru>
2021-12-16 23:06:42 +0300

    FINISH

--- Wallet/ContentView.swift
+++ Wallet/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 HeaderView()
                 CardListVIew()
                 BalanceView()
+                TransferMoneyView()
                 Spacer()
             }.padding(25)
         }
--- Wallet/MockData.swift
+++ Wallet/MockData.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 let cardsData: [Card] = [
-    .init(income: 1245, expenses: 543, cardNumber: "**** 3424", imageName: "visa", isSelected: true),
+    .init(income: 39800, expenses: 543, cardNumber: "**** 3424", imageName: "visa", isSelected: true),
     .init(income: 1334, expenses: 653, cardNumber: "**** 9324", imageName: "mastercard"),
     .init(income: 3108, expenses: 653, cardNumber: "**** 6424", imageName: "american-express")
 ]
