416df8356d53b3d8303bfd14e31a530cb08d93fe
Areg Vardanian <aregv@bk.ru>
2021-12-16 23:01:13 +0300

    TransferMoney view

--- Wallet/BalanceView.swift
+++ Wallet/BalanceView.swift
@@ -28,19 +28,49 @@ struct BalanceView: View {
     var incomeView: some View {
         HStack(spacing: 10) {
             Image(systemName: "arrow.down")
+                .resizable()
+                .frame(width: 20, height: 20)
+                .foregroundColor(Color.primaryPurple)
+                .padding(10)
+                .background(Color.primaryPurple.opacity(0.2))
+                .cornerRadius(10)
+            VStack(alignment: .leading) {
+                Text("In")
+                    .font(.headline)
+                    .foregroundColor(Color(.systemGray3))
+                Text("$\(wallet.selectedCard.income)")
+                    .font(.title2)
+                    .fontWeight(.bold)
+            }
         }
-        Text("In")
     }
     
     var expensesView: some View {
-        Text("Out")
+        HStack(spacing: 10) {
+            Image(systemName: "arrow.up")
+                .resizable()
+                .frame(width: 20, height: 20)
+                .foregroundColor(Color.primaryYellow)
+                .padding(10)
+                .background(Color.primaryYellow.opacity(0.2))
+                .cornerRadius(10)
+            VStack(alignment: .leading) {
+                Text("Out")
+                    .font(.headline)
+                    .foregroundColor(Color(.systemGray3))
+                Text("$\(wallet.selectedCard.expenses)")
+                    .font(.title2)
+                    .fontWeight(.bold)
+            }
+        }
     }
     
     
     var body: some View {
-        VStack {
+        VStack(spacing: 15) {
             headerView
             HorizontalProgressView(percentage: $incomePercentage)
+                .padding(.bottom, 15)
             HStack {
                 incomeView
                 Spacer()
@@ -66,5 +96,7 @@ struct BalanceView_Previews: PreviewProvider {
     static var previews: some View {
         BalanceView()
             .environmentObject(Wallet())
+            .fixedSize(horizontal: false, vertical: true)
+            .padding()
     }
 }
