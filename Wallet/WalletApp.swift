cc64e2367af01da28189b7ba84e344c21e3dd693
Areg Vardanian <aregv@bk.ru>
2021-12-16 13:53:48 +0300

    Initial Commit

---
+++ Wallet/WalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletApp.swift
+//  Wallet
+//
+//  Created by Геворг on 16.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct WalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
